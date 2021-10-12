4f8c43a7e83a6374e3f94a5dceb927b592563859
Attituding <84680760+Attituding@users.noreply.github.com>
2021-10-11 20:08:52 -0700

    testing

--- src/player/APIRequests/slothpixelRequestPlayer.js
+++ src/player/APIRequests/slothpixelRequestPlayer.js
@@ -1,57 +1,57 @@
-import { createHTTPRequest } from '../../utility.js';
-
-export async function slothpixelRequestPlayer(player) {
+import { createHTTPRequest } from "../../utility.js";
+export function slothpixelRequestPlayer(player) {
   return Promise.all([
-    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}`, {method: 'GET', mode: 'cors'}, 5000),
-    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}/status`, {}, 5000),
-    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}/recentGames`, {}, 5000),
-  ]).then((data) => {
-    return slothpixelProcessData(data[0], data[1], data[2]);
-  }).catch(err => {
-    if (err.status === 404) err.name = 'NotFoundError';
+    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}`, { "timeout": 5000 }),
+    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}/status`, { "timeout": 5000 }),
+    createHTTPRequest(`https://api.slothpixel.me/api/players/${player}/recentGames`, { "timeout": 5000 })
+  ])
+  .then(data => slothpixelProcessData(data[0], data[1], data[2]))
+  .catch(err => {
+    if (err.status === 404) err.name = "NotFoundError";
     err.message = err.json?.error ?? `HTTP status of ${err.status}`;
-    err.api = 'Slothpixel';
+    err.api = "Slothpixel";
     throw err;
   });
 }
 
+//eslint-disable-next-line complexity, max-lines-per-function, max-statements, max-lines-per-function
 function slothpixelProcessData(playerData, statusData, recentGamesData) {
-  let tzOffset =  new Date().getTimezoneOffset() / 60;
-  let tzOffsetString = `UTC${createOffset(new Date())}`;
+  const tzOffset = new Date().getTimezoneOffset() / 60,
+  tzOffsetString = `UTC${createOffset(new Date())}`,
 
-  let firstLoginTime = new Date(playerData?.first_login ?? 0 + tzOffset).toLocaleTimeString('en-IN', { hour12: true });
-  let firstLoginDate = cleanDate(new Date(playerData?.first_login ?? 0 + tzOffset));
-  let lastLoginTime = new Date(playerData?.last_login ?? 0 + tzOffset).toLocaleTimeString('en-IN', { hour12: true });
-  let lastLoginDate = cleanDate(new Date(playerData?.last_login ?? 0 + tzOffset));
-  let lastLogoutTime =  new Date(playerData?.last_logout ?? 0 + tzOffset).toLocaleTimeString('en-IN', { hour12: true });
-  let lastLogoutDate = cleanDate(new Date(playerData?.last_logout ?? 0 + tzOffset));
+  firstLoginTime = new Date(playerData?.first_login ?? 0 + tzOffset).toLocaleTimeString("en-IN", { "hour12": true }),
+  firstLoginDate = cleanDate(new Date(playerData?.first_login ?? 0 + tzOffset)),
+  lastLoginTime = new Date(playerData?.last_login ?? 0 + tzOffset).toLocaleTimeString("en-IN", { "hour12": true }),
+  lastLoginDate = cleanDate(new Date(playerData?.last_login ?? 0 + tzOffset)),
+  lastLogoutTime = new Date(playerData?.last_logout ?? 0 + tzOffset).toLocaleTimeString("en-IN", { "hour12": true }),
+  lastLogoutDate = cleanDate(new Date(playerData?.last_logout ?? 0 + tzOffset)),
 
-  let lastPlaytime = cleanTime(playerData?.last_logout - playerData?.last_login);
-  
-  let apiData = new Object();
-  apiData.username = playerData?.username ?? '';
-  apiData.possesive = playerData?.username?.endsWith('s') ? `${playerData?.username ?? ''}'` : `${playerData?.username ?? ''}'s`;
+  lastPlaytime = cleanTime(playerData?.last_logout - playerData?.last_login),
+
+  apiData = {};
+  apiData.username = playerData?.username ?? "";
+  apiData.possesive = playerData?.username?.endsWith("s") ? `${playerData?.username ?? ""}'` : `${playerData?.username ?? ""}'s`;
   apiData.uuid = playerData?.uuid ?? null;
   apiData.language = playerData?.language ?? null;
   apiData.version = playerData?.mc_version ?? null;
-  apiData.legacyAPI = playerData?.last_login < 1494864734000 ? true : false;
-  apiData.status = statusData.online && playerData?.last_login > playerData?.last_logout ? 'Online' : !statusData.online && playerData?.last_login < playerData?.last_logout ? 'Offline' : !statusData.online && playerData?.last_login < 1494864734000 ? 'Offline' : null;
-  apiData.isOnline = statusData?.online === true ? true : false; 
+  apiData.legacyAPI = playerData?.last_login < 1494864734000;
+  apiData.status = statusData.online && playerData?.last_login > playerData?.last_logout ? "Online" : !statusData.online && playerData?.last_login < playerData?.last_logout ? "Offline" : !statusData.online && playerData?.last_login < 1494864734000 ? "Offline" : null;
+  apiData.isOnline = statusData?.online === true;
   apiData.utcOffset = playerData?.last_login || playerData?.last_logout ? `${tzOffsetString}` : null;
 
   if (statusData?.online === true) {
-    apiData.offline = {}
+    apiData.offline = {};
     apiData.offline.playtime = null;
     apiData.offline.lastGame = null;
-    apiData.online = {}
+    apiData.online = {};
     apiData.online.gameType = statusData?.game?.type ?? null;
     apiData.online.mode = statusData?.game?.mode ?? null;
     apiData.online.map = statusData?.game?.map ?? null;
   } else {
-    apiData.offline = {}
+    apiData.offline = {};
     apiData.offline.playtime = playerData?.last_login && playerData?.last_login < playerData?.last_logout ? lastPlaytime : null;
     apiData.offline.lastGame = playerData?.last_game ?? null;
-    apiData.online = {}
+    apiData.online = {};
     apiData.online.gameType = null;
     apiData.online.mode = null;
     apiData.online.map = null;
@@ -70,24 +70,25 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.recentGames = [];
   apiData.recentGamesPlayed = 0;
 
-  for (let i = 0; i < recentGamesData?.length && recentGamesData?.[i]; i++) {
+  //eslint-disable-next-line id-length
+  for (let i = 0; i < recentGamesData?.length && recentGamesData?.[i]; i += 1) {
     apiData.recentGames[i] = {};
-    apiData.recentGames[i].startTime = recentGamesData?.[i]?.date ? new Date(recentGamesData?.[i]?.date + tzOffset).toLocaleTimeString('en-IN', { hour12: true }) : null;
+    apiData.recentGames[i].startTime = recentGamesData?.[i]?.date ? new Date(recentGamesData?.[i]?.date + tzOffset).toLocaleTimeString("en-IN", { "hour12": true }) : null;
     apiData.recentGames[i].startDate = recentGamesData?.[i]?.date ? cleanDate(new Date(recentGamesData?.[i]?.date + tzOffset)) : null;
     apiData.recentGames[i].startMS = recentGamesData?.[i]?.date ?? null;
-    apiData.recentGames[i].endTime = recentGamesData?.[i]?.ended ? new Date(recentGamesData?.[i]?.ended + tzOffset).toLocaleTimeString('en-IN', { hour12: true }) : null;
+    apiData.recentGames[i].endTime = recentGamesData?.[i]?.ended ? new Date(recentGamesData?.[i]?.ended + tzOffset).toLocaleTimeString("en-IN", { "hour12": true }) : null;
     apiData.recentGames[i].endDate = recentGamesData?.[i]?.ended ? cleanDate(new Date(recentGamesData?.[i]?.ended + tzOffset)) : null;
     apiData.recentGames[i].endMS = recentGamesData?.[i]?.ended ?? null;
     apiData.recentGames[i].gameLength = recentGamesData?.[i]?.date && recentGamesData?.[i]?.ended ? recentGamesData?.[i]?.ended - recentGamesData?.[i]?.date : null;
     apiData.recentGames[i].gameType = recentGamesData?.[i]?.gameType ?? null;
     apiData.recentGames[i].mode = recentGamesData?.[i]?.mode ?? null;
     apiData.recentGames[i].map = recentGamesData?.[i]?.map ?? null;
-    if (recentGamesData[i]?.date > playerData?.last_login && recentGamesData?.[i]?.date < playerData?.last_logout) {
-      99 >= i ? apiData.recentGamesPlayed++ : !isNaN(apiData.recentGamesPlayed) ? apiData.recentGamesPlayed = `${apiData.recentGamesPlayed++}+` : '';
-    }
+    if (recentGamesData[i]?.date < playerData?.last_login && recentGamesData?.[i]?.date > playerData?.last_logout) return;
+    if (i <= 99) apiData.recentGamesPlayed += 1;
+    else if (!isNaN(apiData.recentGamesPlayed)) apiData.recentGamesPlayed = `${apiData.recentGamesPlayed += 1}+`;
   }
 
-  apiData.bedwars = {}
+  apiData.bedwars = {};
   apiData.bedwars.level = playerData?.stats?.BedWars?.level ?? 0;
   apiData.bedwars.coins = playerData?.stats?.BedWars?.coins ?? 0;
   apiData.bedwars.wins = playerData?.stats?.Bedwars?.wins_bedwars ?? 0;
@@ -96,16 +97,16 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.bedwars.finalKD = playerData?.stats?.BedWars?.final_k_d ?? 0;
   apiData.bedwars.KD = playerData?.stats?.BedWars.k_d ?? 0;
 
-  apiData.duels = {}
+  apiData.duels = {};
   apiData.duels.coins = playerData?.stats?.Duels?.general?.coins ?? 0;
   apiData.duels.cosmetics = playerData?.stats?.Duels.general?.packages?.length ?? 0;
   apiData.duels.KD = playerData?.stats?.Duels?.general?.kd_ratio ?? 0;
   apiData.duels.WL = playerData?.stats?.Duels?.general?.win_loss_ratio ?? 0;
   apiData.duels.wins = playerData?.stats?.Duels?.general?.wins ?? 0;
   apiData.duels.kills = playerData?.stats?.Duels?.general?.kills ?? 0;
-  apiData.duels.deaths = playerData?.stats?.Duels?.general?.deaths ?? 0
+  apiData.duels.deaths = playerData?.stats?.Duels?.general?.deaths ?? 0;
 
-  apiData.blitz = {}
+  apiData.blitz = {};
   apiData.blitz.coins = playerData?.stats?.Blitz?.coins ?? 0;
   apiData.blitz.KD = playerData?.stats?.Blitz?.k_d ?? 0;
   apiData.blitz.WL = playerData?.stats?.Blitz?.win_loss ?? 0;
@@ -113,7 +114,7 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.blitz.kills = playerData?.stats?.Blitz?.kills ?? 0;
   apiData.blitz.deaths = playerData?.stats?.Blitz?.deaths ?? 0;
 
-  apiData.pit = {}
+  apiData.pit = {};
   apiData.pit.gold = playerData?.stats?.Pit?.gold_earned;
   apiData.pit.prestige = playerData?.stats?.Pit?.prestige ?? 0;
   apiData.pit.playtime = playerData?.stats?.Pit?.playtime_minutes;
@@ -123,7 +124,7 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.pit.kills = playerData?.stats?.Pit?.kills ?? 0;
   apiData.pit.deaths = playerData?.stats?.Pit?.deaths ?? 0;
 
-  apiData.skywars = {}
+  apiData.skywars = {};
   apiData.skywars.level = playerData?.stats?.SkyWars?.level;
   apiData.skywars.coins = playerData?.stats?.SkyWars?.coins ?? 0;
   apiData.skywars.KD = playerData?.stats?.SkyWars?.kill_death_ratio ?? 0;
@@ -132,7 +133,7 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.skywars.kills = playerData?.stats?.SkyWars?.kills ?? 0;
   apiData.skywars.deaths = playerData?.stats?.SkyWars?.deaths ?? 0;
 
-  apiData.speedUHC = {}
+  apiData.speedUHC = {};
   apiData.speedUHC.coins = playerData?.stats?.SpeedUHC?.coins ?? 0;
   apiData.speedUHC.KD = playerData?.stats?.SpeedUHC?.kd ?? 0;
   apiData.speedUHC.WL = playerData?.stats?.SpeedUHC?.win_loss ?? 0;
@@ -140,7 +141,7 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.speedUHC.kills = playerData?.stats?.SpeedUHC?.kills ?? 0;
   apiData.speedUHC.deaths = playerData?.stats?.SpeedUHC?.deaths ?? 0;
 
-  apiData.uhc = {}
+  apiData.uhc = {};
   apiData.uhc.level = playerData?.stats?.UHC?.level ?? 0;
   apiData.uhc.coins = playerData?.stats?.UHC?.coins ?? 0;
   apiData.uhc.KD = playerData?.stats?.UHC?.kd ?? 0;
@@ -148,14 +149,14 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.uhc.wins = playerData?.stats?.UHC?.wins ?? 0;
   apiData.uhc.kills = playerData?.stats?.UHC?.kills ?? 0;
   apiData.uhc.deaths = playerData?.stats?.UHC?.deaths ?? 0;
-  apiData.walls = {}
+  apiData.walls = {};
   apiData.walls.coins = playerData?.stats?.Walls?.coins ?? 0;
   apiData.walls.KD = playerData?.stats?.UHC?.kd ?? 0;
   apiData.walls.WL = playerData?.stats?.UHC?.win_loss ?? 0;
   apiData.walls.wins = playerData?.stats?.Walls?.wins ?? 0;
   apiData.walls.kills = playerData?.stats?.Walls?.kills ?? 0;
   apiData.walls.deaths = playerData?.stats?.Walls?.deaths ?? 0;
-  apiData.megaWalls = {}
+  apiData.megaWalls = {};
   apiData.megaWalls.coins = playerData?.stats?.MegaWalls?.coins ?? 0;
   apiData.megaWalls.KD = playerData?.stats?.UHC?.kill_death_ratio ?? 0;
   apiData.megaWalls.WL = playerData?.stats?.UHC?.win_loss_ratio ?? 0;
@@ -163,36 +164,43 @@ function slothpixelProcessData(playerData, statusData, recentGamesData) {
   apiData.megaWalls.kills = playerData?.stats?.MegaWalls?.kills ?? 0;
   apiData.megaWalls.deaths = playerData?.stats?.MegaWalls?.deaths ?? 0;
 
+  //Unsure as to why this doesnt work
+  //eslint-disable-next-line consistent-return
   return apiData;
 }
 
-function cleanTime(ms) { //Takes MS
-  if (ms < 0) return null;
+function cleanTime(ms) {
+  if (ms < 0 || ms === null || isNaN(ms)) return null;
   let seconds = Math.round(ms / 1000);
-  let days = Math.floor(seconds / (24 * 60 * 60));
+  const days = Math.floor(seconds / (24 * 60 * 60));
   seconds -= days * 24 * 60 * 60;
-  let hours = Math.floor(seconds / (60 * 60));
+  const hours = Math.floor(seconds / (60 * 60));
   seconds -= hours * 60 * 60;
-  let minutes = Math.floor(seconds / 60);
+  const minutes = Math.floor(seconds / 60);
   seconds -= minutes * 60;
-  return `${days > 0 ? `${days}d ${hours}h ${minutes}m ${seconds}s` : hours > 0 ? `${hours}h ${minutes}m ${seconds}s` : minutes > 0 ? `${minutes}m ${seconds}s` : `${seconds}s` }`;
+  return days > 0
+    ? `${days}d ${hours}h ${minutes}m ${seconds}s` : hours > 0
+    ? `${hours}h ${minutes}m ${seconds}s` : minutes > 0
+    ? `${minutes}m ${seconds}s` : `${seconds}s`;
 }
 
 function cleanDate(epoch) {
-  let date = epoch.getDate();
-  let month = new Intl.DateTimeFormat('en-US', {month: 'short'}).format(epoch);
-  let year = epoch.getFullYear();
-  return month + " " + date + ", " + year;
+  const date = epoch.getDate(),
+   month = new Intl.DateTimeFormat("en-US", { "month": "short" }).format(epoch),
+   year = epoch.getFullYear();
+  return `${month} ${date}, ${year}`;
 }
 
-function pad(value) { //Yoinked from https://stackoverflow.com/a/13016136 under CC BY-SA 3.0 matching ISO 8601
-  return value < 10 ? '0' + value : value;
+//Yoinked from https://stackoverflow.com/a/13016136 under CC BY-SA 3.0 matching ISO 8601
+function pad(value) {
+  return value < 10 ? `0${value}` : value;
 }
 
-function createOffset(date) { //Yoinked from https://stackoverflow.com/a/13016136 under CC BY-SA 3.0 matching ISO 8601
-  let sign = (date.getTimezoneOffset() > 0) ? "-" : "+";
-  let offset = Math.abs(date.getTimezoneOffset());
-  let hours = pad(Math.floor(offset / 60));
-  let minutes = pad(offset % 60);
-  return sign + hours + ":" + minutes;
+//Yoinked from https://stackoverflow.com/a/13016136 under CC BY-SA 3.0 matching ISO 8601
+function createOffset(date) {
+  const sign = date.getTimezoneOffset() > 0 ? "-" : "+",
+   offset = Math.abs(date.getTimezoneOffset()),
+   hours = pad(Math.floor(offset / 60)),
+   minutes = pad(offset % 60);
+  return `${sign + hours}:${minutes}`;
 }
--- src/player/checkPlayer.js
+++ src/player/checkPlayer.js
@@ -1,100 +1,118 @@
-import { errorHandler, getLocalStorage, setLocalStorage, getSyncStorage } from '../utility.js';
-import { hypixelRequestPlayer } from './APIRequests/hypixelRequestPlayer.js';
-import { slothpixelRequestPlayer } from './APIRequests/slothpixelRequestPlayer.js';
-import { requestUUID } from './APIRequests/requestUUID.js';
-import { detailMessage } from './messageComponents/detailMessage.js';
-import { explanationMessage } from './messageComponents/explanationMessage.js';
+/*eslint-disable id-length */
+/*eslint-disable max-statements */
+import { errorHandler, getLocalStorage, setLocalStorage, getSyncStorage } from "../utility.js";
+import { hypixelRequestPlayer } from "./APIRequests/hypixelRequestPlayer.js";
+import { slothpixelRequestPlayer } from "./APIRequests/slothpixelRequestPlayer.js";
+import { requestUUID } from "./APIRequests/requestUUID.js";
+import { detailMessage } from "./messageComponents/detailMessage.js";
+import { explanationMessage } from "./messageComponents/explanationMessage.js";
 
-const outputElement = document.getElementById('outputElement');
+const outputElement = document.getElementById("outputElement");
 
 errorEventCreate();
 
-document.getElementById('submitPlayer').addEventListener('submit', processPlayer);
+document.getElementById("submitPlayer").addEventListener("submit", processPlayer);
 
-(async () => { //Loads the last searched player on page load. If the last input threw an error, nothing is displayed
+//Loads the last searched player on page load. If the last input threw an error, nothing is displayed
+(async () => {
   try {
-    const userOptions = await getSyncStorage('userOptions');
+    const userOptions = await getSyncStorage("userOptions");
     if (userOptions.persistentLastPlayer === false) return;
-    const playerHistory = await getLocalStorage('playerHistory');
+    const playerHistory = await getLocalStorage("playerHistory");
     if (!playerHistory?.lastSearches[0]?.apiData || playerHistory?.lastSearchCleared === true) return;
     const text = playerDataString(playerHistory?.lastSearches[0]?.apiData, userOptions);
     userOptions.typewriterOutput = false;
-    return await outputField(text, userOptions, outputElement);
-  } catch(err) {
-    return errorHandler(err, outputElement);
+    await outputField(text, userOptions, outputElement);
+  } catch (err) {
+    errorHandler(err, outputElement);
   }
 })();
 
 async function processPlayer(event) {
   event.preventDefault();
-  const playerValue = document.getElementById('playerValue');
-  const submitButton = document.getElementById('submitButton');
-  const input = playerValue.value.replace(/^\s/, ''); //The player name/UUID from <input>
-  let apiData; //Data returned from Slothpixel/Hypixel
+  const playerValue = document.getElementById("playerValue"),
+        submitButton = document.getElementById("submitButton"),
+        //The player name/UUID from <input>
+        input = playerValue.value.replace(/^\s/, "");
 
-  outputElement.textContent = 'Loading..';
-  playerValue.value = '';
+  //eslint-disable-next-line init-declarations
+  let apiData;
+
+  outputElement.textContent = "Loading..";
+  playerValue.value = "";
   submitButton.disabled = true;
-  submitButton.style.cursor = 'not-allowed';
-  
+  submitButton.style.cursor = "not-allowed";
+
   try {
-    const userOptions = await getSyncStorage('userOptions');
-    if (userOptions.useHypixelAPI === true && !userOptions.apiKey) {let x =  new Error(); x.name = 'KeyError'; throw x}
+    const userOptions = await getSyncStorage("userOptions");
+    if (userOptions.useHypixelAPI === true && !userOptions.apiKey) {
+      const newError = new Error();
+      newError.name = "KeyError";
+      throw newError;
+    }
     apiData = await callAPIs(input, userOptions);
     const text = playerDataString(apiData, userOptions);
-    await updatePlayerHistory(input, apiData).catch(x => errorHandler(x, outputElement));
-    await outputField(text, userOptions, outputElement);
+    return outputField(text, userOptions, outputElement).catch(x => errorHandler(x, outputElement));
   } catch (err) {
-    err.input = input; //Assigns input so that the error logger can read it
+    err.input = input;
     return errorHandler(err, outputElement);
+  } finally {
+    return updatePlayerHistory(input, apiData).catch(x => errorHandler(x, outputElement));
   }
 }
 
-async function callAPIs(input, userOptions) { //Returns a object of the requested player or throws an error
+//Returns a object of the requested player or throws an error
+async function callAPIs(input, userOptions) {
+  //eslint-disable-next-line prefer-named-capture-group
   const uuidRegex = /^[0-9a-f]{8}(-?)[0-9a-f]{4}(-?)[1-5][0-9a-f]{3}(-?)[89AB][0-9a-f]{3}(-?)[0-9a-f]{12}$/;
-  if (userOptions.useHypixelAPI === false) return await slothpixelRequestPlayer(input);
-  else if (uuidRegex.test(input)) return await hypixelRequestPlayer(input, userOptions.apiKey);
-  else {
-    const playerUUID = await requestUUID(input);
-    return await hypixelRequestPlayer(playerUUID, userOptions?.apiKey);
-  }
+  if (userOptions.useHypixelAPI === false) return slothpixelRequestPlayer(input);
+  else if (uuidRegex.test(input)) return hypixelRequestPlayer(input, userOptions.apiKey);
+
+  const playerUUID = await requestUUID(input);
+  return hypixelRequestPlayer(playerUUID, userOptions.apiKey);
 }
 
-function playerDataString(apiData, userOptions) { //Returns a formatted message ready to be displayed
+//Returns a formatted message ready to be displayed
+function playerDataString(apiData, userOptions) {
   if (userOptions.paragraphOutput === false) return detailMessage(apiData, userOptions);
-  else return explanationMessage(apiData, userOptions);
+  return explanationMessage(apiData, userOptions);
 }
 
 async function outputField(text, userOptions) {
-  if (userOptions?.typewriterOutput === false) return outputElement.insertAdjacentHTML('afterbegin', text);
-    
-  for (let i = 0; i < text.length; i += 10) { //Typewriter effect
-    outputElement.textContent = '';
-    outputElement.insertAdjacentHTML('afterbegin', text.slice(0, i + 5).replace(/<\/$|<$/, ''));
-    await new Promise(t => setTimeout(t, text.length / 50)); //Seems to be a good balance as Firefox does this alot slower for some reason
+  if (userOptions?.typewriterOutput === false) return outputElement.insertAdjacentHTML("afterbegin", text);
+
+  //Typewriter effect
+  for (let i = 0; i < text.length; i += 10) {
+    outputElement.textContent = "";
+    outputElement.insertAdjacentHTML("afterbegin", text.slice(0, i + 5).replace(/<\/$|<$/, ""));
+    //Seems to be a good balance as Firefox does this alot slower for some reason
+    //eslint-disable-next-line no-await-in-loop
+    await new Promise(t => {
+      setTimeout(t, text.length / 50);
+    });
   }
-  outputElement.textContent = '';
-  outputElement.insertAdjacentHTML('afterbegin', text);
+  outputElement.textContent = "";
+  return outputElement.insertAdjacentHTML("afterbegin", text);
 }
 
 async function updatePlayerHistory(input, apiData) {
-    const thisPlayerHistory = new Object();
+    const thisPlayerHistory = {};
     thisPlayerHistory.input = input ?? null;
     thisPlayerHistory.uuid = apiData?.uuid ?? null;
     thisPlayerHistory.username = apiData?.username ?? null;
     thisPlayerHistory.apiData = apiData ?? null;
     thisPlayerHistory.epoch = `${Date.now()}`;
-  
-    const playerHistory = await getLocalStorage('playerHistory');
+
+    const playerHistory = await getLocalStorage("playerHistory");
     playerHistory?.lastSearches?.unshift(thisPlayerHistory);
     playerHistory?.lastSearches?.splice(100);
     playerHistory.lastSearchCleared = false;
     delete playerHistory?.lastSearches[1]?.apiData;
-    return await setLocalStorage({ 'playerHistory': playerHistory });
+    return setLocalStorage({ "playerHistory": playerHistory });
 }
 
 function errorEventCreate() {
-  window.addEventListener('error', x => errorHandler(x, outputElement, 'Uncaught'));
-  window.addEventListener('unhandledrejection', x => errorHandler(x, outputElement, 'Unhandled Rejection'));
-  window.onerror = () => {return true}; //Hides the default error
+  window.addEventListener("error", error => errorHandler(error, document.getElementById("errorOutput"), error.constructor.name));
+  window.addEventListener("unhandledrejection", error => errorHandler(error, document.getElementById("errorOutput"), error.constructor.name));
+  window.onerror = () => true;
 }
