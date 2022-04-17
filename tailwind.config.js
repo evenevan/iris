d3be9e237e56aa0ad800012198f2c0607ec69e3f
Attituding <84680760+Attituding@users.noreply.github.com>
2022-04-17 11:49:52 -0700

    chore: even more init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['./src/base/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                'youtube-red': '#f40407',
+                'youtube-red-disabled': '#5a0102',
+            },
+        },
+    },
+    plugins: [],
+};
