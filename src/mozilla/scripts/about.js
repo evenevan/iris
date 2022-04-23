b9f0124b5652d969b4ae0e8fb3d32509a78ebe92
Attituding <84680760+Attituding@users.noreply.github.com>
2022-04-22 18:53:19 -0700

    fix: build

--- src/mozilla/scripts/about.js
+++ src/mozilla/scripts/about.js
@@ -2,7 +2,6 @@ import { i18n, } from './utility/i18n.js';
 import { runtime } from './utility/utility.js';
 (async () => {
     i18n([
-        'extensionName',
         'extensionDescription',
     ]);
     const copyright = document.getElementById('aboutCopyright');
