1210acf78a7902018614febceb41848f2564c4c5
Attituding <84680760+Attituding@users.noreply.github.com>
2022-04-25 18:27:23 -0700

    fix: remove useless files fix: firefox manifest default apge fix: firefox manifest * rc-3

--- src/chromium/scripts/i18n.js
+++
@@ -1,6 +0,0 @@
-export function i18n(runtime, ids) {
-    ids.forEach(i18nString => {
-        const element = document.getElementById(i18nString);
-        element.textContent = runtime.i18n.getMessage(i18nString);
-    });
-}
