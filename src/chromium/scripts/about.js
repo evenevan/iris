e428867c8f0c92a35589134869e792b995ab1390
Attituding <84680760+Attituding@users.noreply.github.com>
2022-12-11 12:48:11 -0800

    refactor: gitignore generated files

--- src/chromium/scripts/about.js
+++
@@ -1,7 +0,0 @@
-import { i18n } from './utility/i18n.js';
-import { runtime } from './utility/utility.js';
-i18n(['extensionName', 'extensionDescription']);
-const copyright = document.getElementById('aboutCopyright');
-copyright.innerHTML = runtime.i18n.getMessage('aboutCopyright', [
-    runtime.runtime.getManifest().version,
-]);
